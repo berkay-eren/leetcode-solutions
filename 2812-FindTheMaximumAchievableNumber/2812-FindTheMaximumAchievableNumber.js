9842ae4b80e3d316e0561a7b98ba0b3e6368c6c9
berkay <194712022+berkay-eren@users.noreply.github.com>
2025-08-28 15:45:20 +0300

    Create 2812-FindTheMaximumAchievableNumber

---
+++ 2812-FindTheMaximumAchievableNumber/2812-FindTheMaximumAchievableNumber.js
@@ -0,0 +1,9 @@
+// Last updated: 28/08/2025, 15:45:20
+/**
+ * @param {number} num
+ * @param {number} t
+ * @return {number}
+ */
+var theMaximumAchievableX = function(num, t) {
+    return num+2*t;
+};
