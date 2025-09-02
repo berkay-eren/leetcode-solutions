be379c19d4f1a4f652455eb07e8be025969273cf
berkay <194712022+berkay-eren@users.noreply.github.com>
2025-09-02 15:46:30 +0300

    Create 3110-ScoreofaString

---
+++ 3110-ScoreofaString/3110-ScoreofaString.java
@@ -0,0 +1,10 @@
+// Last updated: 02/09/2025, 15:46:28
+class Solution {
+    public int scoreOfString(String s) {
+        int res = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            res += Math.abs(s.charAt(i) - s.charAt(i + 1));
+        }
+        return res;
+    }
+}
