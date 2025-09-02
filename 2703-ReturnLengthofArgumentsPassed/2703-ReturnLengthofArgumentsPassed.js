a5a19882372fd311e503613b4ef9381d62cccd5a
berkay <194712022+berkay-eren@users.noreply.github.com>
2025-09-02 15:36:40 +0300

    Create 2703-ReturnLengthofArgumentsPassed

---
+++ 2703-ReturnLengthofArgumentsPassed/2703-ReturnLengthofArgumentsPassed.js
@@ -0,0 +1,12 @@
+// Last updated: 02/09/2025, 15:36:38
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
