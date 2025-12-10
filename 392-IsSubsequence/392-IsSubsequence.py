352f09e1261574928b0b6f6977ac623f6d4a3a62
berkay <194712022+berkay-eren@users.noreply.github.com>
2025-12-10 23:53:38 +0000

    Create 392-IsSubsequence

---
+++ 392-IsSubsequence/392-IsSubsequence.py
@@ -0,0 +1,11 @@
+# Last updated: 10/12/2025, 23:53:37
+1class Solution:
+2    def isSubsequence(self, s: str, t: str) -> bool:
+3        i, j = 0, 0
+4
+5        while i < len(s) and j < len(t):
+6            if s[i] == t[j]:
+7                i += 1
+8            j += 1
+9
+10        return True if i == len(s) else False
